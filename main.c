6c88981f73380801bbbe415136503aa44568fc79
MagicCrayon <ylambda@yandex.com>
2015-10-19 15:21:52 -0400

    Project Import

---
+++ main.c
@@ -0,0 +1,119 @@
+/*
+ * main.c
+ *
+ * weather info from. http://weather.noaa.gov/pub/data/observations/metar/decoded/CYYZ.TXT
+ *
+ * ./weather -s cyyz
+ *
+ * ./weather -h
+ * SHOW USAGE
+ *
+ * ./weather
+ * CYYZ
+ * Oct 18, 2015 - 08:00 PM 
+ * Temperature: -2 C
+ * Dew Point: -4 C
+ * Sky conditions: mostly clear
+ *
+ * TODO
+ * - Make sure input city is only 4 chars
+ * - Write the rest of the program
+ *
+ * - Get Input From Parameters on which city need to show, set one by defualt
+ * - On no params just print out weather of specfied city
+ *      - 
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Function Prototypes */
+void usage();
+int showWeather(char *);
+
+int main(int argc, char* argv[])
+{
+    char url[] = "http://weather.noaa.gov/pub/data/observations/metar/decoded/XXXX.TXT";
+    char *city = "CYYZ";                       /* Show This Citys Weather  */
+    char cityArray[3] = {0};                          /* Used When Appending To url[] */
+    int sflag, hflag = 0;                       /* Set Flags                */
+    char *svalue = NULL;                      /* Hold Prefered City       */
+    int c;
+
+    opterr = 0;
+
+    /*
+     * Handle Arguments
+     */
+    while ((c = getopt (argc, argv, "hs:")) != -1)
+    {
+        switch(c)
+        {
+            case 'h':
+                hflag = 1;
+                break;
+            case 's':
+                city = optarg;
+                break;
+            case '?':
+                if (optopt == 's')
+                    fprintf(stderr, "Option -%c requires an argument. \n", optopt);
+                else if (isprint (optopt))
+                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+                else
+                    fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+                return 1;
+            default:
+                exit(-1);
+        }
+    }
+
+
+    /* Usage */
+    if ( hflag == 1)
+    {
+        usage();
+        exit(1);
+    }
+
+    
+    showWeather(city);
+    
+   /* strcpy(cityArray, city);
+
+    printf("\ncityArray: %s\n", cityArray);
+    
+    url[60] = cityArray[0];
+    url[61] = cityArray[1];
+    url[62] = cityArray[2];
+    url[63] = cityArray[3];
+
+    printf("\n%s\n", url);    
+*/
+    
+    return 0;
+}
+
+void usage()
+{
+    printf("Usage: ./main <argument> <paraneter>\n");
+    printf("-h  -   Show Help\n");
+    printf("-s  -   Set City \n");
+}
+
+/*
+ * Good Enough For Now ;)
+ */
+int showWeather(char *cityname)
+{
+    printf("%s\n", cityname);
+    printf("Oct 18, 2015 - 08:00 PM\n");
+    printf("Temperature: -2 C\n");
+    printf("Dew point: -4 C\n");
+    printf("Sky conditions: mostly clear\n");
+    return 1;
+}
