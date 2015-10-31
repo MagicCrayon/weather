d737f4ffa0793fd18006450a104d976971c04d2a
MagicCrayon <ylambda@yandex.com>
2015-10-30 23:26:29 -0400

    New method for retrieving information from temp file
    getline() now sends all data that was read into the temp file into
    a 2D array named weatherInfo. The first line of the array is looped
    over untill the character '(' is found, the following four characters
    are the ICAO.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  *
  * ./weather -h
  * SHOW USAGE
- *
+*
  * ./weather
  * CYYZ
  * Oct 18, 2015 - 08:00 PM 
@@ -23,6 +23,15 @@
  * - On no params just print out weather of specfied city
  *      - 
  *
+ * FIXME
+ *
+ * New Plan:::
+ *
+ * Use cURL fopen thing
+ * Send the html output into the /tmp file
+ * Set up a two dimensinal array
+ * Read each line of the temp into the array
+ *
  */
 
 #include <stdio.h>
@@ -35,31 +44,36 @@
 /* Function Prototypes */
 void usage();
 int showWeather(char *);
+int getLine(FILE* );
+
 //void curlWrite(void, size_t, size_t, void);
 
-/* 
- * Information parsed from the weather site
- * */
-typedef struct info 
+typedef struct weatherPoints
 {
     char *ICAO;
     char *date;
     char *temp;
     char *dewPoint;
     char *skyCond;
-} info;
+} weatherPoints;
+
+struct MemoryStruct
+{
+    char *memory;
+    size_t size;
+};
+
+/* Globals */
+char weatherInfo[20][256] = {0};
 
 int main(int argc, char* argv[])
 {
-    char *city = "CYYZ";                       /* Show This Citys Weather  */
-    int hflag = 0;                            /* Set Flags                */
+    char *city = "CYYZ";                       
+    int hflag = 0;                            
     int c;
 
     opterr = 0;
 
-    /*
-     * Handle Arguments
-     */
     while ((c = getopt (argc, argv, "hs:")) != -1)
     {
         switch(c)
@@ -89,8 +103,6 @@ int main(int argc, char* argv[])
         }
     }
 
-
-    /* Usage */
     if ( hflag == 1)
     {
         usage();
@@ -102,34 +114,79 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void usage()
+void usage() // TODO Fix the msgs in the help
 {
     printf("Usage: ./main <argument> <paraneter>\n");
     printf("-h  -   Show Help\n");
     printf("-s  -   Set City \n");
 }
 
-void curlWrite(void *contents, size_t size, size_t nmemb, void *userp)
+/*
+int sendDataToBuffer()
 {
-    printf("FEFEEFE=============================================\n");
-    printf("%s", contents);
+    NULL;
+}
+*/
+
+/*
+ * The following function came from a cURL example.
+ * http://curl.haxx.se/libcurl/c/getinmemory.html
+ */
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{   
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+    if(mem->memory == NULL)
+    {
+        /* out of memory */
+        printf("not enough memory (realloc returned NULL\n");
+        return 0;
+    }
+
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+
+}
+
+int getLine(FILE* dataFile)
+{
+   
+    char line[256];
+    int currentline = 0;
+
+    while (fgets(line, sizeof(line), dataFile))
+    {
+        strcpy(weatherInfo[currentline], line);
+        currentline++;
+    }
+    return 1;
 }
 
 int showWeather(char *cityname)
 {
 
-    CURL *curl_handel;
-    CURLcode res;
-
-    /*
-     * Replace XXXX with ICAO code
-     */
     const char *url = "http://weather.noaa.gov/pub/data/observations/metar/decoded/XXXX.TXT";
     char urlArray[strlen(url)];
     char *city = cityname;                       
-    char cityArray[4];                          
+    char cityArray[4];          
 
-    
+    CURL *curl_handle;
+    CURLcode res;
+
+    struct MemoryStruct chunk;
+
+    chunk.memory = malloc(1);
+    chunk.size = 0;
+   
+    /*
+     * Replace XXXX with ICAO
+     */
+
     strcpy(urlArray, url); 
     strcpy(cityArray, city);
     
@@ -139,22 +196,99 @@ int showWeather(char *cityname)
     urlArray[62] = cityArray[2];
     urlArray[63] = cityArray[3];
      
-    //printf("\ncityArray: %s\n", cityArray);
-    //printf("\nURL:%s\n", urlArray);
-
     /*
-     * Using cURL get information
+     * Retrieve data 
      */
-    curl_handel = curl_easy_init();
 
-    if(curl_handel)
+    curl_global_init(CURL_GLOBAL_ALL); 
+
+    curl_handle = curl_easy_init();
+
+    curl_easy_setopt(curl_handle, CURLOPT_URL, urlArray);
+
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    res = curl_easy_perform(curl_handle);
+
+    if (res != CURLE_OK)
     {
-        curl_easy_setopt(curl_handel, CURLOPT_URL, urlArray);
-        curl_easy_setopt(curl_handel, CURLOPT_WRITEFUNCTION, curlWrite);
-        res = curl_easy_perform(curl_handel); /* Output It */
-        curl_easy_cleanup(curl_handel);
+           fprintf(stderr, "curl_easy_perform() failed: %s\n", 
+                curl_easy_strerror(res));
+    }
+    else 
+    {
+        //printf("%lu bytes retrieved\n", (long)chunk.size);
+        //printf("%s\n", chunk.memory);
+
+       /*
+        * Write data to temp
+        */
+
+        FILE* weatherTemp = fopen("/tmp/weathertemp.tmp", "w");
+        if (weatherTemp == NULL)
+        {
+            fprintf(stderr, "Error: Opening Temp File Failed");
+            exit(-1);
+        }
+
+        fwrite(chunk.memory, chunk.size, sizeof(chunk.memory), weatherTemp);
+        fclose(weatherTemp);
+        
+        /* 
+         * Get whole data, send to buffer then info struct
+         */
+        
+        weatherTemp = fopen("/tmp/weathertemp.tmp", "rb");
+        if (weatherTemp == NULL)
+        {
+            fprintf(stderr, "Failed to open temp file");
+            exit(-1);
+        }
+
+
+        struct weatherPoints data;
+        getLine(weatherTemp);
+    
+        /*
+         * Get ICAO from first line
+         */
+        
+        size_t length = strlen(weatherInfo); 
+
+        int count = 0;          
+        while (count <= length)
+        {
+            /* Compare array with ascii */ 
+            if (weatherInfo[0][count] == 40)
+            {
+                char ICAO[5] = {0};
+                ICAO[0] = weatherInfo[0][count+1];
+                ICAO[1] = weatherInfo[0][count+2];
+                ICAO[2] = weatherInfo[0][count+3];
+                ICAO[3] = weatherInfo[0][count+4];
+                ICAO[4] = '\0';
+                data.ICAO = ICAO;
+                printf("%s\n", data.ICAO);
+            }
+            count++;
+        }
+
+        
+
     }
 
+    curl_easy_cleanup(curl_handle);
+
+    free(chunk.memory);
+
+    curl_global_cleanup();
 
     return 1;
 }
+
+
+
